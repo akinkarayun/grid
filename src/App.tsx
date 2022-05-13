7193efdde8c4f640e15888eacb2731bf9de7925b
Akin Karayun <akinkarayun1@hotmail.com>
2022-05-13 19:10:35 +0300

    First Grid Layout Example Created

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './common.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="grid">
+      <div className="card">1</div>
+      <div className="card">2</div>
+      <div className="card">3</div>
+      <div className="card">4</div>
+      <div className="card">5</div>
+      <div className="card">6</div>
+      <div className="card">7</div>
+      <div className="card">8</div>
+      <div className="card">9</div>
+      <div className="card">10</div>
+      <div className="card">11</div>
+      <div className="card">12</div>
+    </section>
   );
 }
 
