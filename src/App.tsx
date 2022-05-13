4486c6eb103a710bff9d9f638c34b3ea303fea13
Akin Karayun <akinkarayun1@hotmail.com>
2022-05-13 19:43:59 +0300

    created grid layout for second example

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,37 @@ import './common.css'
 
 function App() {
   return (
-    <section className="grid">
-      <div className="card">1</div>
-      <div className="card">2</div>
-      <div className="card">3</div>
-      <div className="card">4</div>
-      <div className="card">5</div>
-      <div className="card">6</div>
-      <div className="card">7</div>
-      <div className="card">8</div>
-      <div className="card">9</div>
-      <div className="card">10</div>
-      <div className="card">11</div>
-      <div className="card">12</div>
-    </section>
+    //first grid example
+    // <section className="grid">
+    //   <div className="card">1</div>
+    //   <div className="card">2</div>
+    //   <div className="card">3</div>
+    //   <div className="card">4</div>
+    //   <div className="card">5</div>
+    //   <div className="card">6</div>
+    //   <div className="card">7</div>
+    //   <div className="card">8</div>
+    //   <div className="card">9</div>
+    //   <div className="card">10</div>
+    //   <div className="card">11</div>
+    //   <div className="card">12</div>
+    // </section>
+    //second grid example
+    <section className="animatedgrid">
+      <div className="card">a</div>
+      <div className="card">b</div>
+      <div className="card">c</div>
+      <div className="card">d</div>
+      <div className="card">e</div>
+      <div className="card">f</div>
+      <div className="card">g</div>
+      <div className="card">h</div>
+      <div className="card">i</div>
+      <div className="card">j</div>
+      <div className="card">k</div>
+      <div className="card">l</div>
+      <div className="card">main</div>
+    </section >
   );
 }
 
